eaec9121eef735b88e7cf7ff3c915913059e1b2a
mmmeyers <marsi286@gmail.com>
2016-03-25 18:58:38 -0400

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,17 @@
 'use strict';
+
+function favIceCream(iceCream) {
+  return "I love " + iceCream;
+}
+
+function shouting(words) {
+  return words.toUpperCase();
+}
+
+function roundDown(num) {
+  return Math.floor(num);
+}
+
+function theTruth() {
+  return true;
+}
